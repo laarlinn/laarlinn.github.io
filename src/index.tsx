b29936b6b9d2da5b926c75dee45407e218caa6bb
Lauri Linna <lauri.a.linna@student.jyu.fi>
2020-11-05 17:25:00 +0200

    Update

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-import './index.css';
-import Root from './components/Root';
-
-render(<Root />, document.getElementById("appRoot"));
